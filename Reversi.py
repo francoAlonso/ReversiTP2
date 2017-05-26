6025dd30709ce3b81590b9414cd931b51aeb02f1
francoAlonso <fra.n.co@hotmail.com>
2017-05-26 14:55:21 -0300

    Se agrego el archivo funciones
    A partir de ahora se usara el archivo funciones.py para almacenar todas
    las funciones que importara el programa principal reversi.py. En
    funciones estan las variables constantes y las funciones dibujar_tablero
    e inicializar_tablero

---
+++ Reversi.py
@@ -0,0 +1,9 @@
+import funciones
+
+tablero = [
+    ["","","","","","","","","",""] ,
+    ["","","","","","","","","",""]
+]
+
+tablero = funciones.inicializar_tablero(tablero)
+funciones.dibujar_tablero(tablero)
