14ac08add312c4877beac5b6fb95621e6a875d44
AlvaroIribarren <yoalvy@live.com.ar>
2017-06-15 11:28:53 -0300

    Create menuUsuarios.py

--- menuUsuarios.py
+++ menuUsuarios.py
@@ -1,19 +1,21 @@
 def grabarUsuario(archivo,nombre,puntaje,PJ,PG,PP,PE,espacio):
 usuarios.write(nombre+','+puntaje+','+PJ+','+PG+','+PP+','+PE+espacio)
 
+
 def accionARealizar():
 	ingreso=raw_input('Ingrese la acción a realizar')
 	return ingreso
 
-
+#Resetea el archivo borrando todos los usuarios
 def resetear():
     if ingreso=RESETEAR:
-		usuarios=open('usuarios.csv', 'w')
-		leer(usuarios, 'NONE,0,0,0,0,0,0')
+	usuarios=open('usuarios.csv', 'w')
+	leer(usuarios, 'NONE,0,0,0,0,0,0')
         while linea in usuarios:
             usuarios.remove(linea)
             leer(usuarios, 'NONE,0,0,0,0,0,0')
 
+#Genera la cantidad de usuarios aleatorios que el usuario desee
 def usuariosAleatorios():
 	import random
         generados=input('¿Cuántos usuarios de forma aleatoria desea generar?')
@@ -23,6 +25,11 @@ def usuariosAleatorios():
 			puntaje, PJ, PG, PP, PE=random.randrange(10), random.randrange(10), random.randrange(10),random.randrange(10),random.randrange(10)
 			usuarios.seek(0,2)
 			grabarUsuario(usuarios, nombre_aleatorio, puntaje, PJ, PG, PP, PE,'')
+			
+#Visualizar todos los usuarios con sus respectivos valores (mostrado alfabeticamente)
+def visualizar():
+        
+
 	
 
 #"main"
