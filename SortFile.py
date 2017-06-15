cb646cd8168d93b40ce7b0230126648501d42cee
Handerllon <manuel121097@gmail.com>
2017-06-15 08:51:20 -0300

    Create SortFile.py

--- SortFile.py
+++ SortFile.py
@@ -10,7 +10,7 @@ def crearLista (file): #Pasa los datos de Usuarios.csv a una lista para luego se
 def ordenarLista (listaInicial): #Se le pasa la lista creada anteriormente........HAY QUE HACER SPLIT PARA QUE PUEDA ORDENAR. VER ESTO
 	lista=sorted(listaInicial, key=lambda k:int(k['Puntaje']), reverse=True)#ordena por puntaje de mayor a menor
 	j=1
-	for i, line in enumerate(lista):
+	for i in range(0,(len(lista)-1)):
 		if  lista[j-1]['Puntaje']==lista[j]['Puntaje']: #Si en el registro de un jugador el puntaje es igual al del siguiente
 			if int(lista[j-1]['PartGanados'])<int(lista[j]['PartGanados']): #Si el PG del segundo es mas grande que el del primero los cambia de lugar
 				aux=lista[j-1]	
@@ -19,12 +19,11 @@ def ordenarLista (listaInicial): #Se le pasa la lista creada anteriormente......
 			if int(lista[j-1]['PartGanados'])==int(lista[j]['PartGanados']): #Si sus PG son iguales, ingresa los nombres en una lista pequena y los ordena alfabeticamente
 				lst=[lista[j-1]['Nombre'],lista[j]['Nombre']]
 				lst.sort()
-				print lst
 				if lista[j]['Nombre']==lst[0]: #Si el primer nombre de la lst es igual al siguiente de la lista usuarios, cambia de lugar
 					aux=lista[j-1]
 					lista[j-1]=lista[j]
 					lista[j]=aux
-		j=+1
+		j=j+1
 	return lista
 
 def imprimirLista(lista):
