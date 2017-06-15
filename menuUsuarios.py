c5aa08e30fc0bf3211e16165428a896fffc93bcb
AlvaroIribarren <yoalvy@live.com.ar>
2017-06-15 01:19:36 -0300

    Update menuUsuarios.py
    Pequeños errores

--- menuUsuarios.py
+++ menuUsuarios.py
@@ -1,3 +1,6 @@
+def grabarUsuario(archivo,nombre,puntaje,PJ,PG,PP,PE,espacio):
+usuarios.write(nombre+','+puntaje+','+PJ+','+PG+','+PP+','+PE+espacio)
+
 def accionARealizar():
 	ingreso=raw_input('Ingrese la acción a realizar')
 	return ingreso
@@ -13,14 +16,19 @@ def resetear():
 
 def usuariosAleatorios():
 	import random
-    if ingreso=GENERAR_RANDOM:
         generados=input('¿Cuántos usuarios de forma aleatoria desea generar?')
         for i in range [1, generados+1]
 			nombre_aleatorio=[random.choice("abcdefghijklmnopqrstuvwxyz") for j in range(4)]
 			nombre_aleatorio=''.join(nombre_aleatorio)
-			lista=[nombre_aleatorio, random.randrange(10), random.randrange(10), random.randrange(10),random.randrange(10),random.randrange(10)]
+			puntaje, PJ, PG, PP, PE=random.randrange(10), random.randrange(10), random.randrange(10),random.randrange(10),random.randrange(10)
 			usuarios.seek(0,2)
-			usuarios.write(lista)
+			grabarUsuario(usuarios, nombre_aleatorio, puntaje, PJ, PG, PP, PE)
 	
+
+#"main"
 ingreso=accionARealizar()
-			
+if ingreso=RESETEAR:
+	resetear()
+elif ingreso=RANDOM:
+	usuariosAleatorios()
+		
