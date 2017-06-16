9d5440096c34134e8e4c4e6c80d9dd223cf57ef9
Handerllon <manuel121097@gmail.com>
2017-06-16 09:12:42 -0300

    Agregado clear screen

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -1,4 +1,5 @@
 #Programa Principal, desde aqui haremos el menu
+import os
 menu1=['1-Jugar','2-Ver Top 10','3-Cargar,generar o actualizar archivo Usuarios','0-Salir']
 menu2=['1-Reset archivo Usuarios','2-Cargar nuevo arhivo Usuarios','3-Actualizar archivo Usuarios','4-Generar Usuarios aleatorio','0-Volver al Menu Principal']
 i=1
@@ -37,8 +38,10 @@ while i!=0:
                 #Vuelve al menu principal
             else: #Accion invalida
                 print ('Por favor ingresar un numero valido')
+            os.system('cls')
     elif i==0:
         print 'algo'
         #Como no hace nada sale del programa
     else: #Accion invalida
         print ('Por favor ingresar un numero valido')
+    os.system('cls')
