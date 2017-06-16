ed890a4731555edc408d7420ced70902fe3d6d96
Handerllon <manuel121097@gmail.com>
2017-06-16 09:05:35 -0300

    Arreglada la indedntacion

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -4,41 +4,41 @@ menu2=['1-Reset archivo Usuarios','2-Cargar nuevo arhivo Usuarios','3-Actualizar
 i=1
 while i!=0:
     for k in menu1:
-            print k,'\n'
+        print k,'\n'
     j=1
     i=input('Por favor ingresar la accion a realizar acorde al numero correspondiente\n')
     if i==1:
-                print 'algo' #Se inicia el juego
+        print 'algo' #Se inicia el juego
     elif i==2:
         #Se imprime el top 10 y vuelve
-                #Aca va el programa de top 10
-                while j!=0:
-                        j=input('Presione 0 para volver al menu principal\n')
+        #Aca va el programa de top 10
+        while j!=0:
+            j=input('Presione 0 para volver al menu principal\n')
     elif i==3:
-            #Va a menuUsuarios
+        #Va a menuUsuarios
         while j!=0:
             for k in menu2:
                 print k,'\n'
             j=input('Por favor ingresar la accion a realizar acorde al numero correspondiente\n')
             if j==1:
-                                print 'algo'
-                                #Resetea Usuarios, se borra el anterior
+                print 'algo'
+                #Resetea Usuarios, se borra el anterior
             elif j==2:
-                                print 'algo'
-                                #Carga nuevo Usuarios, se borra el anterior
+                print 'algo'
+                #Carga nuevo Usuarios, se borra el anterior
             elif j==3:
-                                print 'algo'
+                print 'algo'
                 #Permite crear un nuevo novUsuarios y realiza el apareo
             elif j==4:
-                                print 'algo'
+                print 'algo'
                 #Genera un Usuarios aleatorio, se borra el anterior
             elif j==0:
-                                print 'algo'
-                                #Vuelve al menu principal
-            else: #Accion invalida
-                                print ('Por favor ingresar un numero valido')
-    elif i==0:
                 print 'algo'
+                #Vuelve al menu principal
+            else: #Accion invalida
+                print ('Por favor ingresar un numero valido')
+    elif i==0:
+        print 'algo'
         #Como no hace nada sale del programa
     else: #Accion invalida
         print ('Por favor ingresar un numero valido')
