f34f1b809f6970a2d71ba63b2c7b760bda57d488
Handerllon <manuel121097@gmail.com>
2017-06-19 12:11:32 -0300

    Agregadas todas las funciones en menu
    Fui metiendo todo lo que hicimos, ni en pedo funciona de una pero cuando metamos el juego vamos viendo como lo arreglamos

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -1,7 +1,12 @@
 #Programa Principal, desde aqui haremos el menu
 import os
+os.chdir()#Aca se pone desde donde se van a leer los archivos, para que no sea tanto lio
+#Vamos a usar el leer y el grabar de menuJugar, cuando compilemos todo borramos los leer y grabar repetidos
+import MenuJugar
+import SortFile
+import menuUsuarios
 menu1=['1-Jugar','2-Ver Top 10','3-Cargar,generar o actualizar archivo Usuarios','0-Salir']
-menu2=['1-Reset archivo Usuarios','2-Cargar nuevo arhivo Usuarios','3-Actualizar archivo Usuarios','4-Generar Usuarios aleatorio','0-Volver al Menu Principal']
+menu2=['1-Reset archivo Usuarios','2-Cargar nuevo arhivo Usuarios','3-Actualizar archivo Usuarios','4-Generar Usuarios aleatorio','5-Visualizar archivo Usuarios actual','0-Volver al Menu Principal']
 i=1
 while i!=0:
     for k in menu1:
@@ -9,10 +14,14 @@ while i!=0:
     j=1
     i=input('Por favor ingresar la accion a realizar acorde al numero correspondiente\n')
     if i==1:
-        print 'algo' #Se inicia el juego
-    elif i==2:
-        #Se imprime el top 10 y vuelve
-        #Aca va el programa de top 10
+        varParaNombre=MenuJugar.ingresarNombre()#Habria que hacer que verifique e ingrese en uno solo
+        MenuJugar.verificarUsuarioInicial('Usuarios.csv',varParaNombre)
+        #ACA VA EL JUEGO
+        MenuJugar.actualizarUsuarios('Usuarios.csv',varParaNombre,fichasJugaor,fichasBot)
+    elif i==2: #Ordenamiento top 10
+        listaParaOrdenar=SortFile.crearLista('Usuarios.csv')
+        listaUsuarios=SortFile.ordenarLista(listaParaOrdenar)
+        SortFile.imprimirLista('Usuarios.csv',listaUsuarios)
         while j!=0:
             j=input('Presione 0 para volver al menu principal\n')
     elif i==3:
@@ -21,20 +30,18 @@ while i!=0:
             for k in menu2:
                 print k,'\n'
             j=input('Por favor ingresar la accion a realizar acorde al numero correspondiente\n')
-            if j==1:
-                print 'algo'
-                #Resetea Usuarios, se borra el anterior
-            elif j==2:
-                print 'algo'
-                #Carga nuevo Usuarios, se borra el anterior
-            elif j==3:
-                print 'algo'
-                #Permite crear un nuevo novUsuarios y realiza el apareo
-            elif j==4:
-                print 'algo'
-                #Genera un Usuarios aleatorio, se borra el anterior
+            if j==1: #Resetear archivo usuarios, borra el anterior
+                menuUsuarios.resetearArchUsuarios('Usuarios.csv')
+            elif j==2: #Cargar nuevo archivo usuarios, borra el anterior
+                menuUsuarios.cargarArchUsuarios('Usuarios.csv')
+            elif j==3: #Crear novUsuarios para luego aparearlo con Usuarios.csv
+                menuUsuarios.novedadesUsuarios('novUsuarios.csv')
+                menuUsuarios.apareoNovedades('Usuarios.csv','novUsuarios.csv')
+            elif j==4: #Crea usuarios Aleatorios, borra el anterior
+                menuUsuarios.usuariosAleatorios('Usuarios.csv')
+            elif j==5: #Imprime todos los usuarios ordenados por nombre
+                menuUsuarios.visualizar('Usuarios.csv')
             elif j==0:
-                print 'algo'
                 #Vuelve al menu principal
             else: #Accion invalida
                 print ('Por favor ingresar un numero valido')
